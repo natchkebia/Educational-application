d00b2a4191f57f39f5e5fc0a9da372af8e0bec66
Tamar <tamarnatchkebia2@gmail.com>
2024-07-14 22:39:25 +0400

    feat: courses section

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const slides = document.querySelector('.slider-container');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let currentIndex = 0;
+
+function updateSlider() {
+    const offset = -currentIndex * (270 + 24);
+    slides.style.transform = `translateX(${offset}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex > slides.children.length - 4) { 
+        currentIndex = 0; 
+    }
+    updateSlider();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.children.length - 4; 
+    }
+    updateSlider();
+});
