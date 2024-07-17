9034ac0a175ad1a4f35cd4d475361466e4742a6e
nino <ninosopoian@gmail.com>
2024-07-17 14:14:44 +0400

    feat: add burger bar functionality
    - Implemented `openMobile` function to handle mobile menu opening
    - Added responsive design to burger menu
    - Fixed class names  for burger menu

---
+++ js/mobileMenu.js
@@ -0,0 +1,11 @@
+// mobile-menu
+
+// const mobileMenu = document.getElementById("mobile-menu");
+// const navMenu = document.querySelector("nav ul");
+// const mainContent = document.querySelector("body");
+
+function openMobileMenu() {
+  document.getElementById("mobile-menu").classList.toggle("show");
+  document.getElementById("burger-icon").classList.toggle("active");
+
+}
