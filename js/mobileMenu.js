d544f0c6f53cd467fdd0481f34327f41f47a3f61
nino <ninosopoian@gmail.com>
2024-07-19 14:51:12 +0400

    fix: background inactivating,  when mobile menu is opened. - added overflow:hidden for body

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -2,13 +2,11 @@
 
 function openMobileMenu() {
   document.getElementById("mobile-menu").classList.toggle("show");
-    document.getElementById("burger-icon").classList.toggle("active");
-       document.getElementById("main").classList.toggle("hide");
- 
+  document.getElementById("burger-icon").classList.toggle("active");
+  document.body.classList.toggle("inactive");
 }
 
 // function for showing profile in mobile menu and "log-out" add class "show", and hide authorization-link - add class hide
-//
 
 // function showProfile() {
 //       document.getElementById("profile").classList.add("show");
