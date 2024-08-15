9fcd80c182387bea2657fcd5d345ee8ba2cd57a9
nino <ninosopoian@gmail.com>
2024-08-15 19:04:10 +0400

    feat: add courses detail page - about and syllabus content

--- js/course-details-tab.js
+++ js/course-details-tab.js
@@ -26,3 +26,37 @@ function openTab(evt, tabName) {
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".tablinks").click();
 });
+
+// course accordion
+let dropdowns = document.getElementsByClassName(
+  "syllabus__accordion--question"
+);
+let i;
+
+for (i = 0; i < dropdowns.length; i++) {
+  dropdowns[i].addEventListener("click", function () {
+    let isActive = this.classList.contains("open");
+
+    closeAllDropdowns();
+
+    if (!isActive) {
+      this.classList.toggle("open");
+      let dropdownContent = this.nextElementSibling;
+      dropdownContent.style.display = "block";
+
+      let height = dropdownContent.scrollHeight + "px";
+      dropdownContent.style.maxHeight = height;
+    }
+  });
+}
+
+function closeAllDropdowns() {
+  for (i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].classList.remove("open");
+    let dropdownContent = dropdowns[i].nextElementSibling;
+    dropdownContent.style.display = "none";
+    dropdownContent.style.maxHeight = null;
+  }
+}
+
+drop();
