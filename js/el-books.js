151ddbb6025edc90e2d5fa9600bbd3c7ec618865
nino <ninosopoian@gmail.com>
2024-09-06 02:45:05 +0400

    feat: add see more functionaliti inside of acordion

--- js/el-books.js
+++ js/el-books.js
@@ -16,21 +16,58 @@ document.querySelectorAll(".gallery__btn").forEach((btn) => {
 });
 
 // acordion
-
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll(".accordion__section");
 
   sections.forEach((section) => {
-    section.addEventListener("click", function () {
-      // Toggle "down" class only for the clicked section
+    const title = section.querySelector(".accordion__title");
+    // Event listener for the accordion title
+    title.addEventListener("click", function (event) {
+      event.stopPropagation();
+
+      // Toggle "show-content" class for the clicked section
       section.classList.toggle("show-content");
 
-      // Optionally close other sections
+      // Close other sections
       sections.forEach((otherSection) => {
         if (otherSection !== section) {
-          otherSection.classList.remove("down");
+          otherSection.classList.remove("show-content");
+          otherSection
+            .querySelector(".more__content")
+            .classList.remove("show-more");
+          const otherButtons = otherSection.querySelectorAll(".more__button");
+          otherButtons.forEach((button) => {
+            button.textContent = "იხილე მეტი";
+            button.classList.remove("hidden");
+          });
         }
       });
     });
+  });
+});
+
+// see more btn
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("toggleButton");
+  const textContent = document.querySelector(".accordion__text");
+  const buttonText = document.getElementById("buttonText");
+  const toggleIcon = document.getElementById("toggleIcon");
+
+  let isExpanded = false;
+
+  toggleButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    isExpanded = !isExpanded;
+
+    if (isExpanded) {
+      textContent.classList.add("expanded");
+      buttonText.textContent = "იხილე ნაკლები";
+      toggleIcon.classList.add("rotate");
+    } else {
+      textContent.classList.remove("expanded");
+      buttonText.textContent = "იხილე მეტი";
+      toggleIcon.classList.remove("rotate");
+    }
   });
 });
